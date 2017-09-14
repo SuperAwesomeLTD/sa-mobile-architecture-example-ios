aa4d71358af944ba7a48c90f8be9f67b2b05f316
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-09-14 17:49:29 +0100

    Update

--- SAMobileArchitecture/State.swift
+++ SAMobileArchitecture/State.swift
@@ -1,13 +1,32 @@
 //
-//  State.swift
+//  AppState.swift
 //  SAMobileArchitecture
 //
-//  Created by Gabriel Coman on 08/09/2017.
+//  Created by Gabriel Coman on 14/09/2017.
 //  Copyright © 2017 Gabriel Coman. All rights reserved.
 //
 
 import UIKit
 
-protocol State {
-    // do nothing
+protocol State {}
+
+struct AppState: State {
+    var mainState: MainState
+    var introState: IntroState
+    
+    static func initial() -> AppState {
+        return AppState(mainState: MainState(data: [], isLoading: false, hasError: false),
+                        introState: IntroState(shouldNavigate: false))
+    }
 }
+
+struct IntroState: State {
+    var shouldNavigate: Bool
+}
+
+struct MainState: State {
+    var data: [BackendModel]
+    var isLoading: Bool
+    var hasError: Bool
+}
+
